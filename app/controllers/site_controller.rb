8a8cf77591d4036d53d7bacd524b78096b1cb551
skji <jishankai@qq.com>
2017-08-31 18:39:17 +0800

    update

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,3 +1,18 @@
 class SiteController < ApplicationController
   layout false
+  
+  def index
+  end
+
+  def teacher
+  end
+
+  def course
+  end
+
+  def books
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'site#index'
 
+  get 'index' => 'site#index'
+  get 'books' => 'site#books'
+  get 'course' => 'site#course'
+  get 'teacher' => 'site#teacher'
+  get 'about' => 'site#about'
+
   match ':controller(/:action(/:id(.:format)))', :via => :all
 
   resource :wechat, only: [:show, :create]
