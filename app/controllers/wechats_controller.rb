5d7d25986707da190612410debb5fc2ce7dbe540
skji <jishankai@qq.com>
2017-10-16 12:10:44 +0800

    update wechatscontroller

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -2,6 +2,10 @@ class WechatsController < ApplicationController
   # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#wechat_responder---rails-responder-controller-dsl
   wechat_responder
 
+  def index
+
+  end
+  
   # 默认文字信息responder
   on :text do |request, content|
     request.reply.text "echo: #{content}" #Just echo
